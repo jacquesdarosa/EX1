a4b7d21ddd20f91ddb826e7d0134fc816dee5d92
Jacques <jacquesdarosa@gmail.com>
2022-08-09 14:12:11 +0200

    change1

---
+++ exercise.py
@@ -0,0 +1,9 @@
+# do exercise here : 
+# Task1 :printing single objects
+print('bash')
+
+
+
+
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
