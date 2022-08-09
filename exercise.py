fb963a529c19bf55594a20c8724612a6c1cd888d
Jacques <jacquesdarosa@gmail.com>
2022-08-09 15:45:37 +0200

    Change3

--- exercise.py
+++ exercise.py
@@ -1,6 +1,31 @@
 # do exercise here : 
 # Task1 :printing single objects
+
+
 print('bash')
+print(234)
+
+x = str("bash")
+print(x)
+
+
+y = int(234)
+print(y)
+
+z = float(45.29)
+print(z)
+
+print(type(3+2j))
+
+print('Hello')
+
+print(bool(True))
+
+print(int(123),float(43.23),complex(4-1j),str('How are you?'),bool(True),sep= "-")
+
+print(isinstance('city', str),isinstance("city", int))
+
+print(isinstance(123, int),isinstance(43.23, bool),isinstance(4-1j, complex), isinstance(True, bool),isinstance('how are you?', float))
 
 
 
