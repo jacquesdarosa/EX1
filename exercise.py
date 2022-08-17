449a7efea2793d52b60c11918f5f6979cb1a078f
Jacques <jacquesdarosa@gmail.com>
2022-08-17 15:34:17 +0200

    ex6

--- exercise.py
+++ exercise.py
@@ -2,10 +2,10 @@
 # Task1 :printing single objects
 
 
-print('bash')
+
 print(234)
 
-x = str("bash")
+x = str("Hello")
 print(x)
 
 
@@ -15,18 +15,33 @@ print(y)
 z = float(45.29)
 print(z)
 
+# Task 2
+
 print(type(3+2j))
+print(type(43.23))
+print(type("how are you doing"))
+
+print(type(True))
+
 
 print('Hello')
+
 
 print(bool(True))
 
 print(int(123),float(43.23),complex(4-1j),str('How are you?'),bool(True),sep= "-")
 
+# Task 3
 print(isinstance('city', str),isinstance("city", int))
+
+# Task 4
 
 print(isinstance(123, int),isinstance(43.23, bool),isinstance(4-1j, complex), isinstance(True, bool),isinstance('how are you?', float))
 
+# Task 5
+# This a comment about using block comments
+print(4+5j) # j is a complex number
+print (bool(False)) # False is class bool
 
 
 
